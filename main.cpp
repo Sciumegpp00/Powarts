5b6341f0f8d91989d9c8b156f38047b70ad64574
Alessandro Zanola <ales.zanola@gmail.com>
2020-12-09 19:32:07 +0100

    Init main function to read input file and write to output file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+
+//class Graph
+
+
+int N;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream in("input.txt");
+    in >> N;
+    cout << N << '\n';
+//    auto l = new list<int>();
+
+    for (int i = 0; i < N; i++) {
+        // TODO: Add to graph
+    }
+
+    ofstream out("output.txt");
+    // TODO: Write output
+    in.close();
+    out.close();
     return 0;
 }
